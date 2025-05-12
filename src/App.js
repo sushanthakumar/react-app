1535ebc278d8ac9433ced81a1cfabfcda984e0dd
sushanthakumar <sushanth.github@gmail.com>
2025-05-13 01:19:46 +0530

    webapp

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello, World!</h1>
+    </div>
+  );
+}
+
+export default App;
+
